6e3df131ee642223bb42b7132d9692429e9fce51
s.simon <s.simon@levi9.com>
2017-03-02 10:10:43 +0200

    created some containers and routing

---
+++ components/MainView.js
@@ -0,0 +1,9 @@
+import React, { Component } from 'react';
+
+class MainView extends Component {
+  render() {
+    return <div>THIS IS MAIN VIEW</div>;
+  }
+}
+
+export default MainView;
--- main.js
+++ main.js
@@ -1,10 +1,23 @@
 
 import ReactDOM from 'react-dom';
 import React from 'react';
+import {IndexRoute, Router, Route, browserHistory} from 'react-router';
+import App from './components/App';
+import Header from './components/Header';
+import MainView from './components/MainView';
+
 import 'jquery';
 
+const routes = (
+      <Router history={browserHistory}>
+        <Route path="/" component={App}>
+          <IndexRoute component={Header}/>
+          <Route path="/home" component={MainView} />
+        </Route>
+      </Router>
+);
 
 ReactDOM.render(
-  <div>Hello</div>,
+  routes,
   document.getElementById('root')
 );
