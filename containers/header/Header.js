2946167ff2720aac6a3bc6649b71c08c9de87a09
s.simon <s.simon@levi9.com>
2017-03-06 13:54:00 +0200

    responsive and animation

--- containers/header/Header.js
+++ containers/header/Header.js
@@ -2,30 +2,49 @@ import React, { Component } from 'react';
 import './Header.scss';
 
 class Header extends Component {
+
+  toggleMenu = (action) => {
+
+    const menu = document.getElementsByClassName("header-container-menu-mobile")[0];
+    switch (action) {
+      case 'toggle':
+        $(menu).slideToggle();
+        break;
+      case 'close':
+        $(menu).slideUp();
+        break;
+      default: break;
+    }
+  }
+
   render() {
     return (
-        <header>
+        <header className="header">
           <div className="header-container">
-            <div className="header-container-logo">
-              <img src="https://portfolios.kpu.ca/artefact/file/download.php?file=92121&view=37215https://portfolios.kpu.ca/artefact/file/download.php?file=92121&view=37215" />
+
+            <div className="header-container-title">
+              <a className="header-container-logo">
+                <img src="https://portfolios.kpu.ca/artefact/file/download.php?file=92121&view=37215https://portfolios.kpu.ca/artefact/file/download.php?file=92121&view=37215" />
+              </a>
+              <div className="icon header-container-button" onClick={() => this.toggleMenu('toggle')}>
+                <div className="icon-burger">
+                </div>
+              </div>
             </div>
+
             <div className="header-container-menu">
-                <ul className="header-container-menu-ul">
+                <ul className="header-container-menu-desktop">
                   <li className="link"> Despre </li>
                   <li className="link"> Inscriere </li>
                   <li className="link"> Tehnologii </li>
                   <li className="link"> Contact </li>
                 </ul>
-
-                <div className="header-container-menu-ul-tablet">
-                  <div className="icon">
-                    <div className="icon-burger"></div>
-                  </div>
-                </div>
-
-                <div className="header-container-menu-ul-mobile">
-                  mobile
-                </div>
+                <ul className="header-container-menu-mobile" onMouseLeave={() => this.toggleMenu('close')} onClick={() => this.toggleMenu('close')}>
+                  <li className="link"> Despre </li>
+                  <li className="link"> Inscriere </li>
+                  <li className="link"> Tehnologii </li>
+                  <li className="link"> Contact </li>
+                </ul>
 
             </div>
           </div>
