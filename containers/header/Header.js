a4a9fe627535eeee462c4162f7dad0a9748f8d42
s.simon <s.simon@levi9.com>
2017-03-07 12:52:18 +0200

    added icons

--- containers/header/Header.js
+++ containers/header/Header.js
@@ -24,28 +24,49 @@ class Header extends Component {
 
             <div className="header-container-title">
               <a className="header-container-logo">
-                <img src="https://portfolios.kpu.ca/artefact/file/download.php?file=92121&view=37215https://portfolios.kpu.ca/artefact/file/download.php?file=92121&view=37215" />
+                <img src="https://scontent.fotp3-2.fna.fbcdn.net/v/t31.0-8/14715450_387428041600877_4956678542402155955_o.png?oh=228f94a935e279ee81e97a24acf9ab25&oe=597073E8" />
               </a>
-              <div className="icon header-container-button" onClick={() => this.toggleMenu('toggle')}>
-                <div className="icon-burger">
+              <div className="header-container-button" onClick={() => this.toggleMenu('toggle')}>
+                <div className="icon icon-burger">
                 </div>
               </div>
             </div>
 
             <div className="header-container-menu">
                 <ul className="header-container-menu-desktop">
-                  <li className="link"> Despre </li>
-                  <li className="link"> Inscriere </li>
-                  <li className="link"> Program </li>
-                  <li className="link"> Tehnologii </li>
-                  <li className="link"> Contact </li>
+                  <li className="link">
+                    Despre <span className="icon icon-info"></span>
+                  </li>
+                  <li className="link">
+                    Inscriere <span className="icon icon-sign-in"></span>
+                  </li>
+                  <li className="link">
+                    Program <span className="icon icon-calendar"></span>
+                  </li>
+                  <li className="link">
+                    Tehnologii <span className="icon icon-tools"></span>
+                  </li>
+                  <li className="link">
+                    Contact <span className="icon icon-contact"></span>
+                  </li>
                 </ul>
+
                 <ul className="header-container-menu-mobile" onMouseLeave={() => this.toggleMenu('close')} onClick={() => this.toggleMenu('close')}>
-                  <li className="link"> Despre </li>
-                  <li className="link"> Inscriere </li>
-                  <li className="link"> Program </li>
-                  <li className="link"> Tehnologii </li>
-                  <li className="link"> Contact </li>
+                  <li className="link">
+                    Despre <span className="icon icon-info"></span>
+                  </li>
+                  <li className="link">
+                    Inscriere <span className="icon icon-sign-in"></span>
+                  </li>
+                  <li className="link">
+                    Program <span className="icon icon-calendar"></span>
+                  </li>
+                  <li className="link">
+                    Tehnologii <span className="icon icon-tools"></span>
+                  </li>
+                  <li className="link">
+                    Contact <span className="icon icon-contact"></span>
+                  </li>
                 </ul>
             </div>
           </div>
