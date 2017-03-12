24a2680b4543a38e172e96f7748928ce67ad4d97
AlexMitan <alex.mitan@yahoo.com>
2017-03-12 18:51:44 +0200

    Basic component

--- containers/about/About.js
+++ containers/about/About.js
@@ -1,10 +1,61 @@
 import React, { Component } from 'react';
 import './About.scss';
 
+
+class Card extends Component{
+    render(){
+        const {name, imgSrc, role} = this.props;
+        return(
+            <div className="card">
+                <img src={imgSrc}/>
+                <div className="card__desc">
+                    <p className="card__name">{name}</p>
+                    <p className="card__role">{role}</p>
+                </div>
+            </div>
+        );
+    }
+}
+
+const dummy = [
+    {
+        name: "Apo",
+        imgSrc: "http://cdn1.aradon.ro/2014/11/Sfantul-Andrei.jpg",
+        role: "bauss",
+    },{
+        name: "Alex",
+        imgSrc: "http://i2.kym-cdn.com/entries/icons/facebook/000/012/443/Naamloos-2_bewerkt-1.jpg",
+        role: "middle-end-dev",
+    },{
+        name: "Stefi",
+        imgSrc: "http://www.thedailymash.co.uk/images/stories/hipster4254.jpg",
+        role: "Doamna Front-end"
+    },{
+        name: "",
+        imgSrc: "",
+        role: "Haker",
+    }
+];
+
+for (var i = 0; i < 30; i++) {
+    dummy.push({
+        name: "Alex" + i,
+        imgSrc: "http://i2.kym-cdn.com/entries/icons/facebook/000/012/443/Naamloos-2_bewerkt-1.jpg",
+        role: "middle-end-dev",}
+    );
+}
+
 class About extends Component {
     render() {
         return(
-            <div>
+            <div className="temp-container">
+                <h1 className="about__title">About Us</h1>
+                <h1 className="about__title">Meet the team!</h1>
+                <div className="card-container">
+                    {dummy.map(
+                        (e) => (<Card key={e.name} name={e.name} imgSrc={e.imgSrc} role={e.role}/>)
+                    )}
+                </div>
             </div>
         );
     }
--- containers/home/Home.js
+++ containers/home/Home.js
@@ -1,11 +1,12 @@
 import React, { Component } from 'react';
-
+import About from '../about/About';
 class Home extends Component {
   render() {
     return (
         <div id="home">
           <div className="home-container">
             title and other info
+            <About/>
           </div>
         </div>
     );
